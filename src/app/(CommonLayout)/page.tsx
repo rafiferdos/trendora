071fa873e50fcbc5b398b8266953f203a64165d1
hossain <hossainahamedk@gmail.com>
2025-04-18 15:04:17 +0600

    added shadcn

--- src/app/(CommonLayout)/page.tsx
+++ src/app/(CommonLayout)/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button"
 
 const Homepage = () => {
     return (
         <div>
             <h1>Welcome to SwapNest homepage</h1>
+            <Button>Click me</Button>
         </div>
     );
 };
