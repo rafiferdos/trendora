49bdeda3351864d61602723c88f6f66dbbb3364b
Humayun1318 <humayunKabir6267@gmail.com>
2025-04-21 13:14:18 +0600

    problem solved

--- src/services/listings/index.ts
+++ src/services/listings/index.ts
@@ -2,16 +2,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ApiResponse, TListing } from "@/types/listings/listing";
 import { revalidateTag } from "next/cache";
+import { cookies } from "next/headers";
 
 
 
 export async function getListings(): Promise<ApiResponse<TListing[]>> {
-  const res = await fetch("http://localhost:5000/api/v1/listings", {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/listings`, {
     next: {
       tags: ["LISTINGS"],
     },
   });
-// console.log(await res.json());
+
   if (!res.ok) {
     throw new Error("Failed to fetch listings");
   }
@@ -19,14 +20,29 @@ export async function getListings(): Promise<ApiResponse<TListing[]>> {
   return res.json();
 }
 
+// single fetch data by id
+export const getSingleListing = async (id: string): Promise<ApiResponse<TListing>> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/listings/${id}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${(await cookies()).get("accessToken")?.value}`, 
+    },
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch listing");
+  }
+  return res.json();
+};
+
+
 //Add Listing - sends raw JSON
 export const addListingItem = async (listingData: Record<string, any>): Promise<any> => {
   try {
-    const res = await fetch(`http://localhost:5000/api/v1/listings`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/listings`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        // Authorization: (await cookies()).get("accessToken")!.value, // if needed
+        Authorization: `Bearer ${(await cookies()).get("accessToken")?.value}`, // if needed
       },
       body: JSON.stringify(listingData),
     });
@@ -37,16 +53,42 @@ export const addListingItem = async (listingData: Record<string, any>): Promise<
     return Error(error);
   }
 };
+//Update Listing - 
+export const updateListingItem = async (
+  id: string,
+  updatedData: Partial<TListing>
+): Promise<any> => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/listings/${id}`, {
+      method: "PUT", 
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${(await cookies()).get("accessToken")?.value}`,
+      },
+      body: JSON.stringify(updatedData),
+    });
+    console.log(res);
+
+    if (!res.ok) {
+      throw new Error("Failed to update listing");
+    }
+
+    return res.json();
+  } catch (error: any) {
+    return Error(error);
+  }
+};
+
 
 //for the delete service
 export const deleteListingItem = async (id: string): Promise<any> => {
+  console.log(id);
   try {
-    const res = await fetch(`http://localhost:5000/api/v1/listings/${id}`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/listings/${id}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
-        "x-user-id": "test-user-123",    // mocked userId
-        "x-user-role": "user",           // or "admin" if you want admin permissions
+        Authorization: `Bearer ${(await cookies()).get("accessToken")?.value}`,
       },
     });
 
